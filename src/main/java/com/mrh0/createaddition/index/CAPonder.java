98e950a83a2b1fd5457960bcf6a26f171c03ff27
MRH0 <mrhmail1@gmail.com>
2021-08-11 00:49:30 +0200

    20210811a

--- src/main/java/com/mrh0/createaddition/index/CAPonder.java
+++ src/main/java/com/mrh0/createaddition/index/CAPonder.java
@@ -2,6 +2,7 @@ package com.mrh0.createaddition.index;
 
 import com.mrh0.createaddition.CreateAddition;
 import com.mrh0.createaddition.ponder.PonderScenes;
+import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 import com.simibubi.create.foundation.ponder.PonderRegistry;
 import com.simibubi.create.foundation.ponder.content.PonderTag;
 import net.minecraft.util.ResourceLocation;
@@ -10,16 +11,18 @@ public class CAPonder {
 	public static final PonderTag ELECTRIC = new PonderTag(new ResourceLocation(CreateAddition.MODID, "electric")).item(CABlocks.ELECTRIC_MOTOR.get(), true, false)
 			.defaultLang("Electric", "Components which use electricity");
 	
+	static final PonderRegistrationHelper HELPER = new PonderRegistrationHelper(CreateAddition.MODID);
+	
 	public static void register() {
-		PonderRegistry.startRegistration(CreateAddition.MODID);
+		//PonderRegistry..(CreateAddition.MODID);
 
-		PonderRegistry.addStoryBoard(CABlocks.ELECTRIC_MOTOR, "electric_motor", PonderScenes::electricMotor, PonderTag.KINETIC_SOURCES, ELECTRIC);
-		PonderRegistry.addStoryBoard(CABlocks.ALTERNATOR, "alternator", PonderScenes::alternator, PonderTag.KINETIC_APPLIANCES, ELECTRIC);
-		PonderRegistry.addStoryBoard(CABlocks.ROLLING_MILL, "rolling_mill", PonderScenes::rollingMill, PonderTag.KINETIC_APPLIANCES);
-		PonderRegistry.addStoryBoard(CABlocks.ROLLING_MILL, "automate_rolling_mill", PonderScenes::automateRollingMill, PonderTag.KINETIC_APPLIANCES);
-		PonderRegistry.addStoryBoard(CABlocks.HEATER, "heater", PonderScenes::heater, PonderTag.LOGISTICS, ELECTRIC);
+		HELPER.addStoryBoard(CABlocks.ELECTRIC_MOTOR, "electric_motor", PonderScenes::electricMotor, PonderTag.KINETIC_SOURCES, ELECTRIC);
+		HELPER.addStoryBoard(CABlocks.ALTERNATOR, "alternator", PonderScenes::alternator, PonderTag.KINETIC_APPLIANCES, ELECTRIC);
+		HELPER.addStoryBoard(CABlocks.ROLLING_MILL, "rolling_mill", PonderScenes::rollingMill, PonderTag.KINETIC_APPLIANCES);
+		HELPER.addStoryBoard(CABlocks.ROLLING_MILL, "automate_rolling_mill", PonderScenes::automateRollingMill, PonderTag.KINETIC_APPLIANCES);
+		HELPER.addStoryBoard(CABlocks.HEATER, "heater", PonderScenes::heater, PonderTag.LOGISTICS, ELECTRIC);
 		if(CreateAddition.CC_ACTIVE)
-			PonderRegistry.addStoryBoard(CABlocks.ELECTRIC_MOTOR, "cc_electric_motor", PonderScenes::ccMotor, PonderTag.KINETIC_SOURCES, ELECTRIC);
+			HELPER.addStoryBoard(CABlocks.ELECTRIC_MOTOR, "cc_electric_motor", PonderScenes::ccMotor, PonderTag.KINETIC_SOURCES, ELECTRIC);
 		
 		PonderRegistry.TAGS.forTag(PonderTag.KINETIC_SOURCES)
 			.add(CABlocks.ELECTRIC_MOTOR);
@@ -36,6 +39,6 @@ public class CAPonder {
 			.add(CABlocks.ALTERNATOR)
 			.add(CABlocks.HEATER);
 
-		PonderRegistry.endRegistration();
+		//PonderRegistry.endRegistration();
 	}
 }
