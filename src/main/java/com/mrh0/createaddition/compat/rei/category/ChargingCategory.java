d37913dfe154e3ea17863ff7428b5dbf35ac794f
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2022-08-06 17:27:35 +1000

    Mega oops

--- src/main/java/com/mrh0/createaddition/compat/rei/category/ChargingCategory.java
+++ src/main/java/com/mrh0/createaddition/compat/rei/category/ChargingCategory.java
@@ -1,31 +1,31 @@
 package com.mrh0.createaddition.compat.rei.category;
 
-import java.util.List;
-
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mrh0.createaddition.recipe.rolling.RollingRecipe;
+import com.mrh0.createaddition.recipe.charging.ChargingRecipe;
+import com.mrh0.createaddition.util.Util;
 import com.simibubi.create.compat.rei.display.CreateDisplay;
 import com.simibubi.create.foundation.gui.AllGuiTextures;
-
 import me.shedaniel.math.Point;
 import me.shedaniel.rei.api.client.gui.widgets.Widget;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.ItemStack;
 
-public class RollingMillCategory extends CARecipeCategory<RollingRecipe> {
+import java.util.List;
 
-    private final AnimatedRollingMill rolling_mill = new AnimatedRollingMill();
+public class ChargingCategory extends CARecipeCategory<ChargingRecipe> {
 
-    public RollingMillCategory(Info<?> info) {
-        super((Info<RollingRecipe>) info, "charging");
+    private final AnimatedTeslaCoil tesla_coil = new AnimatedTeslaCoil();
+
+    public ChargingCategory(Info<?> info) {
+        super((Info<ChargingRecipe>) info, "Tesla");
     }
 
-
     @Override
-    public void addWidgets(CreateDisplay<RollingRecipe> display, List<Widget> ingredients, Point origin) {
+    public void addWidgets(CreateDisplay<ChargingRecipe> display, List<Widget> ingredients, Point origin) {
         ingredients.add(basicSlot(origin.x + 15, origin.y + 9)
                 .markInput()
-                .entries(EntryIngredients.ofIngredient(display.getRecipe().getIngredient())));
+                .entries(EntryIngredients.of(display.getRecipe().ingredient.getItems()[0])));
 
         ItemStack result = display.getRecipe().getResultItem();
         int yOffset = 0;
@@ -36,12 +36,13 @@ public class RollingMillCategory extends CARecipeCategory<RollingRecipe> {
     }
 
     @Override
-    public void draw(RollingRecipe recipe, PoseStack matrixStack, double mouseX, double mouseY) {
+    public void draw(ChargingRecipe recipe, PoseStack matrixStack, double mouseX, double mouseY) {
         AllGuiTextures.JEI_SLOT.render(matrixStack, 14, 8);
         AllGuiTextures.JEI_ARROW.render(matrixStack, 85, 32);
         AllGuiTextures.JEI_DOWN_ARROW.render(matrixStack, 43, 4);
-        rolling_mill.draw(matrixStack, 48, 27);
+        tesla_coil.draw(matrixStack, 48, 27);
+
+        Minecraft.getInstance().font.draw(matrixStack, Util.format(recipe.energy) + "fe", 86, 9, 4210752);
 
         getRenderedSlot(recipe, 0).render(matrixStack, 139, 27);
     }
-}
