b6a3c169569b482f0891d0c1ae4bd72122b134f5
Grauly <nils.dellemann@web.de>
2022-08-09 18:56:10 +0200

    feat: made Sequenced Assembly Description Translatable

--- src/main/java/com/mrh0/createaddition/recipe/rolling/RollingRecipe.java
+++ src/main/java/com/mrh0/createaddition/recipe/rolling/RollingRecipe.java
@@ -13,6 +13,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -110,7 +111,7 @@ public class RollingRecipe extends ProcessingRecipe<RecipeWrapper> implements IA
 
 	@Override
 	public Component getDescriptionForAssembly() {
-		return new TextComponent("Rolling in Mill").withStyle(ChatFormatting.DARK_GREEN);
+		return new TranslatableComponent("createaddition.recipe.rolling.sequence").withStyle(ChatFormatting.DARK_GREEN);
 	}
 
 	@Override
