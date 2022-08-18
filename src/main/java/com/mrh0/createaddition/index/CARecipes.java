101bb975842ed2f6ca0a55821d56abe81a5c4b35
Grauly <nils.dellemann@web.de>
2022-08-18 14:52:44 +0200

    fix: replaced recipe serializer for compat

--- src/main/java/com/mrh0/createaddition/index/CARecipes.java
+++ src/main/java/com/mrh0/createaddition/index/CARecipes.java
@@ -5,8 +5,8 @@ import com.mrh0.createaddition.recipe.charging.ChargingRecipe;
 import com.mrh0.createaddition.recipe.charging.ChargingRecipeSerializer;
 import com.mrh0.createaddition.recipe.conditions.FluidTagEmptyCondition;
 import com.mrh0.createaddition.recipe.rolling.RollingRecipe;
-import com.mrh0.createaddition.recipe.rolling.RollingRecipeSerializer;
-
+import com.mrh0.createaddition.recipe.rolling.RollingRecipeProcessingFactory;
+import com.mrh0.createaddition.recipe.rolling.SequencedAssemblyRollingRecipeSerializer;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeSerializer;
@@ -14,15 +14,15 @@ import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.event.RegistryEvent;
 
 public class CARecipes {
-	public static void register(RegistryEvent.Register<RecipeSerializer<?>> event) {
-		event.getRegistry()
-			.register(new RollingRecipeSerializer().setRegistryName(new ResourceLocation(CreateAddition.MODID, "rolling")));
-		Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(CreateAddition.MODID, "rolling"), RollingRecipe.TYPE);
-		
-		event.getRegistry()
-		.register(new ChargingRecipeSerializer().setRegistryName(new ResourceLocation(CreateAddition.MODID, "charging")));
-		Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(CreateAddition.MODID, "charging"), ChargingRecipe.TYPE);
-		
-		CraftingHelper.register(FluidTagEmptyCondition.Serializer.INSTANCE);
-	}
+    public static void register(RegistryEvent.Register<RecipeSerializer<?>> event) {
+        event.getRegistry()
+                .register(new SequencedAssemblyRollingRecipeSerializer(new RollingRecipeProcessingFactory()).setRegistryName(new ResourceLocation(CreateAddition.MODID, "rolling")));
+        Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(CreateAddition.MODID, "rolling"), RollingRecipe.TYPE);
+
+        event.getRegistry()
+                .register(new ChargingRecipeSerializer().setRegistryName(new ResourceLocation(CreateAddition.MODID, "charging")));
+        Registry.register(Registry.RECIPE_TYPE, new ResourceLocation(CreateAddition.MODID, "charging"), ChargingRecipe.TYPE);
+
+        CraftingHelper.register(FluidTagEmptyCondition.Serializer.INSTANCE);
+    }
 }
--- src/main/java/com/mrh0/createaddition/recipe/rolling/RollingRecipe.java
+++ src/main/java/com/mrh0/createaddition/recipe/rolling/RollingRecipe.java
@@ -1,7 +1,6 @@
 package com.mrh0.createaddition.recipe.rolling;
 
 import com.mrh0.createaddition.CreateAddition;
-
 import com.mrh0.createaddition.compat.jei.RollingMillAssemblySubCategory;
 import com.mrh0.createaddition.index.CABlocks;
 import com.simibubi.create.compat.jei.category.sequencedAssembly.SequencedAssemblySubCategory;
@@ -27,102 +26,102 @@ import java.util.function.Supplier;
 
 public class RollingRecipe extends ProcessingRecipe<RecipeWrapper> implements IAssemblyRecipe {
 
-	protected final ItemStack output;
-	protected final ResourceLocation id;
-	protected final Ingredient ingredient;
-	
-	public static RecipeType<RollingRecipe> TYPE = new RollingRecipeType();
-	@SuppressWarnings("deprecation")
-	public static RecipeSerializer<?> SERIALIZER = Registry.RECIPE_SERIALIZER.get(new ResourceLocation(CreateAddition.MODID, "rolling"));
+    public static RecipeType<RollingRecipe> TYPE = new RollingRecipeType();
+    @SuppressWarnings("deprecation")
+    public static RecipeSerializer<?> SERIALIZER = Registry.RECIPE_SERIALIZER.get(new ResourceLocation(CreateAddition.MODID, "rolling"));
+    protected final ItemStack output;
+    protected final ResourceLocation id;
+    protected final Ingredient ingredient;
 
-	protected RollingRecipe(Ingredient ingredient, ItemStack output, ResourceLocation id) {
-		super(new RollingRecipeInfo(id, (RollingRecipeSerializer) SERIALIZER,TYPE),new RollingMillRecipeParams(id,ingredient,new ProcessingOutput(output,1f)));
-		this.output = output;
-		this.id = id;
-		this.ingredient = ingredient;
-	}
-	
-	public Ingredient getIngredient() {
-		return ingredient;
-	}
-	
-	@Override
-	public boolean matches(RecipeWrapper inv, Level worldIn) {
-		if (inv.isEmpty())
-			return false;
-		return ingredient.test(inv.getItem(0));
-	}
+    protected RollingRecipe(Ingredient ingredient, ItemStack output, ResourceLocation id) {
+        super(new RollingRecipeInfo(id, (SequencedAssemblyRollingRecipeSerializer) SERIALIZER, TYPE), new RollingMillRecipeParams(id, ingredient, new ProcessingOutput(output, 1f)));
+        this.output = output;
+        this.id = id;
+        this.ingredient = ingredient;
+    }
 
-	@Override
-	protected int getMaxInputCount() {
-		return 1;
-	}
+    public static void register() {
+    }
 
-	@Override
-	protected int getMaxOutputCount() {
-		return 100;
-	}
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
 
-	@Override
-	public ItemStack assemble(RecipeWrapper inv) {
-		return this.output;
-	}
+    @Override
+    public boolean matches(RecipeWrapper inv, Level worldIn) {
+        if (inv.isEmpty())
+            return false;
+        return ingredient.test(inv.getItem(0));
+    }
 
-	@Override
-	public boolean canCraftInDimensions(int width, int height) {
-		return width * height > 0;
-	}
+    @Override
+    protected int getMaxInputCount() {
+        return 1;
+    }
 
-	@Override
-	public ItemStack getResultItem() {
-		return this.output;
-	}
+    @Override
+    protected int getMaxOutputCount() {
+        return 1;
+    }
 
-	@Override
-	public ResourceLocation getId() {
-		return this.id;
-	}
+    @Override
+    public ItemStack assemble(RecipeWrapper inv) {
+        return this.output;
+    }
 
-	@Override
-	public RecipeSerializer<?> getSerializer() {
-		
-		return SERIALIZER;
-	}
+    @Override
+    public boolean canCraftInDimensions(int width, int height) {
+        return width * height > 0;
+    }
 
-	@Override
-	public RecipeType<?> getType() {
-		return TYPE;
-	}
-	
-	@Override
-	public ItemStack getToastSymbol() {
-		return this.output;
-	}
-	
-	@Override
-	public boolean isSpecial() {
-		return true;
-	}
-	
-	public static void register() {};
+    @Override
+    public ItemStack getResultItem() {
+        return this.output;
+    }
 
-	@Override
-	public Component getDescriptionForAssembly() {
-		return new TranslatableComponent("createaddition.recipe.rolling.sequence").withStyle(ChatFormatting.DARK_GREEN);
-	}
+    @Override
+    public ResourceLocation getId() {
+        return this.id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+
+        return SERIALIZER;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return TYPE;
+    }
+
+    @Override
+    public ItemStack getToastSymbol() {
+        return this.output;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return true;
+    }
 
 	@Override
-	public void addRequiredMachines(Set<ItemLike> set) {
-		set.add(CABlocks.ROLLING_MILL.get());
-	}
+    public Component getDescriptionForAssembly() {
+        return new TranslatableComponent("createaddition.recipe.rolling.sequence").withStyle(ChatFormatting.DARK_GREEN);
+    }
 
-	@Override
-	public void addAssemblyIngredients(List<Ingredient> list) {
+    @Override
+    public void addRequiredMachines(Set<ItemLike> set) {
+        set.add(CABlocks.ROLLING_MILL.get());
+    }
 
-	}
+    @Override
+    public void addAssemblyIngredients(List<Ingredient> list) {
 
-	@Override
-	public Supplier<Supplier<SequencedAssemblySubCategory>> getJEISubCategory() {
-		return () -> RollingMillAssemblySubCategory::new;
-	}
+    }
+
+    @Override
+    public Supplier<Supplier<SequencedAssemblySubCategory>> getJEISubCategory() {
+        return () -> RollingMillAssemblySubCategory::new;
+    }
 }
