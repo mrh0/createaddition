ef2ed7b61b97dc7ae7bafd5ec4961b1ecbfb76a7
Fluffy Bumblebee <60066663+FluffyBumblebees@users.noreply.github.com>
2023-02-11 01:29:42 +1100

    ComputerCraft fixes

---
+++ src/main/java/com/mrh0/createaddition/compat/computercraft/ComputerCraftCompat.java
@@ -0,0 +1,10 @@
+package com.mrh0.createaddition.compat.computercraft;
+
+import dan200.computercraft.api.ComputerCraftAPI;
+
+public class ComputerCraftCompat {
+    public static void registerCompat() {
+        ComputerCraftAPI.registerPeripheralProvider(new PeripheralProvider());
+
+    }
+}
