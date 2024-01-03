6b6abb0bf3dfb17a2e8605550ab93dc42ae06ac6
blue <midnight@nightmarish.blue>
2024-01-03 15:50:59 +0000

    Add the registration for electric motor buzz.

--- src/main/java/com/mrh0/createaddition/sound/CASounds.java
+++ src/main/java/com/mrh0/createaddition/sound/CASounds.java
@@ -1,14 +1,23 @@
 package com.mrh0.createaddition.sound;
 
 import com.mrh0.createaddition.CreateAddition;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class CASounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, CreateAddition.MODID);
+
+    public static final RegistryObject<SoundEvent> ELECTRIC_MOTOR_BUZZ = registerSoundEvent("electric_motor_buzz");
+
+    private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
+        ResourceLocation id = new ResourceLocation(CreateAddition.MODID, name);
+        return SOUND_EVENTS.register(name, () -> new SoundEvent(id));
+    }
 
     public static void register(IEventBus eventBus) {
         SOUND_EVENTS.register(eventBus);
