b9ea1128672e5314abd43d0fb494be226f6e5ef9
skippyall <>
2024-02-13 20:06:40 +0100

    Merge fixes

--- src/main/java/com/mrh0/createaddition/index/CASounds.java
+++ src/main/java/com/mrh0/createaddition/index/CASounds.java
@@ -3,6 +3,7 @@ package com.mrh0.createaddition.index;
 import com.mrh0.createaddition.CreateAddition;
 import io.github.fabricators_of_create.porting_lib.util.LazyRegistrar;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
@@ -10,7 +11,7 @@ import java.util.function.Supplier;
 
 public class CASounds {
     public static final LazyRegistrar<SoundEvent> SOUND_EVENTS =
-            LazyRegistrar.create(Registry.SOUND_EVENT, CreateAddition.MODID);
+            LazyRegistrar.create(Registries.SOUND_EVENT, CreateAddition.MODID);
 
     public static final Supplier<SoundEvent> ELECTRIC_MOTOR_BUZZ = registerSoundEvent("electric_motor_buzz");
     public static final Supplier<SoundEvent> TESLA_COIL = registerSoundEvent("tesla_coil");
@@ -20,7 +21,7 @@ public class CASounds {
 
     private static Supplier<SoundEvent> registerSoundEvent(String name) {
         ResourceLocation id = new ResourceLocation(CreateAddition.MODID, name);
-        return SOUND_EVENTS.register(name, () -> new SoundEvent(id));
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(id));
     }
 
     public static void register() {
